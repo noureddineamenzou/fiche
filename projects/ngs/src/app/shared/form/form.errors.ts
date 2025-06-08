61f7b9020b247846a48306d3decaa5ed15ddf94e
noureddineamenzou <noureddine.amenzou@usms.ac.ma>
2025-06-08 21:25:49 +0100

    English To French (Login)

--- projects/ngs/src/app/shared/form/form.errors.ts
+++ projects/ngs/src/app/shared/form/form.errors.ts
@@ -6,35 +6,35 @@ export class Ai1FormValidatorError {
         name: string
     ): string {
         if (ctrl.hasError('required')) {
-            return `${camelCaseToText(name).toUpperCase()} is required`;
+            return `${camelCaseToText(name).toUpperCase()} est requis`;
         } else if (ctrl.hasError('pattern')) {
             return `${camelCaseToText(
                 name
             ).toUpperCase()} value is not permitted`;
         } else if (ctrl.hasError('minlength')) {
-            return `${camelCaseToText(name).toUpperCase()} is short than ${
+            return `${camelCaseToText(name).toUpperCase()} est plus court que ${
                 ctrl.errors?.['minlength'].requiredLength
             }`;
         } else if (ctrl.hasError('maxlength')) {
-            return `${camelCaseToText(name).toUpperCase()} is long than ${
+            return `${camelCaseToText(name).toUpperCase()} est plus long que ${
                 ctrl.errors?.['maxlength'].requiredLength
             }`;
         } else if (ctrl.hasError('email')) {
-            return `${camelCaseToText(name).toUpperCase()} is not an email`;
+            return `${camelCaseToText(name).toUpperCase()} n'est pas une adresse e-mail valide`;
         } else if (ctrl.hasError('min')) {
             return `${camelCaseToText(
                 name
-            ).toUpperCase()} should be greater than ${
+            ).toUpperCase()} doit être supérieur à ${
                 ctrl.errors?.['min'].min
             }`;
         } else if (ctrl.hasError('max')) {
             return `${camelCaseToText(
                 name
-            ).toUpperCase()} should be smaller than ${
+            ).toUpperCase()} doit être inférieur à ${
                 ctrl.errors?.['max'].max
             }`;
         } else {
-            return `${camelCaseToText(name).toUpperCase()} has error`;
+            return `${camelCaseToText(name).toUpperCase()} contient une erreur`;
         }
     }
 }
